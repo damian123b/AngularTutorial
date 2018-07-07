ec54ac92dcdf1c543bb318514f26fc2780d041a5
damian123b <damian_bober@interia.pl>
2018-07-07 19:56:26 +0200

    przrgladanie listy dziala

---
+++ toh/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from "./hero";
+
+export const HEROES : Hero[] = [
+{id: 2, name: "Marian"},
+{id: 3, name: "Zenek"},
+{id: 4, name: "Arnold"},
+{id: 5, name: "Stefan"},
+{id: 6, name: "Gniewomir"},
+{id: 7, name: "Damian"}
+];
